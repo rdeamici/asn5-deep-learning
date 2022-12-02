11fc149ec449b5c74145b41fb59d7edc40e9b64e
rdeamici <rtdeamicis@gmail.com>
2022-12-01 18:50:28 -0800

    adding comparison script, made p5_classification re-usable

---
+++ compare_models.py
@@ -0,0 +1,87 @@
+from p5_classification import classify as p5_classify
+import os
+
+class Classifier:
+    def __init__(self, name):
+        self.path_to_classifiers = "caffe_models/image_classification/"
+        self.path_to_images = "caffe_models/images/"
+        self.labels = "synset_words.txt"
+        
+        self.path_to_classifier = self.path_to_classifiers+name
+        self._prototext = None
+        self._model = None
+        self._image = None
+        self.result_image = None
+        self.total_time = None
+        self.cl_labels = None
+        self.probabilities = None
+
+    @property
+    def prototext(self):
+        return self._prototext
+    @prototext.setter
+    def prototext(self, filename):
+        self._prototext = self.path_to_classifier+filename
+
+
+    @property
+    def model(self):
+        return self._model
+    @model.setter
+    def model(self, filename):
+        self._model = self.path_to_classifier+filename
+
+    @property
+    def image(self):
+        return self._image
+    @image.setter
+    def image(self, filename):
+        self._image = self.path_to_images+filename
+
+    def classify(self):
+        self.result_image, self.total_time, self.cl_labels, self.probabilities = p5_classify(self)
+
+
+def compare():
+    classifiers = ["alexnet","googlenet","mobilenet","resnet_101","shufflenet"]
+    path_to_images = "caffe_models/images/"
+    results = []
+
+    for image in os.listdir(path_to_images):
+        for classifier in classifiers:
+            classifier = Classifier(classifier)
+            classifier.image = image
+            for file in os.listdir(classifier.path_to_classifier):
+                if "prototext" in file:
+                    classifier.prototext = file
+                elif "caffemodel" in file:
+                    classifier.model = file
+
+                classifier.image = image
+                classifier.classify()
+                results.append(classifier)
+    return results
+
+def write_results_to_file(results):
+    header = ["image", "classifier","label", "probability","total_time"]
+    with open("model_results.csv", "w") as f:
+        print(",".join(header), file=f)
+    
+    delimiter = ", "
+    with open("model_results.csv", "a") as f:
+        for classifier in results:
+            for cl_label, probability in zip(classifier.cl_labels, classifier.probabilities):
+                f.write(classifier.image)
+                f.write(delimiter+classifier.name)
+                f.write(delimiter+cl_label)
+                f.write(delimiter+probability)
+                f.write(delimiter+classifier.total_time)
+                f.write("\n")
+
+
+def main():
+    results = compare()
+
+
+if __name__ == "__main__":
+    main()
--- p5_classification.py
+++ p5_classification.py
@@ -10,61 +10,78 @@ import numpy as np
 import argparse
 import time
 import cv2
+import sys
 
-# construct the argument parse and parse the arguments
-ap = argparse.ArgumentParser()
-ap.add_argument("-i", "--image", required=True,
-                help="path to input image")
-ap.add_argument("-p", "--prototxt", required=True,
-                help="path to Caffe 'deploy' prototxt file")
-ap.add_argument("-m", "--model", required=True,
-                help="path to Caffe pre-trained model")
-ap.add_argument("-l", "--labels", required=True,
-                help="path to ImageNet labels (i.e., synsets)")
-args = vars(ap.parse_args())
+def classify(args):
+    # load the input image from disk
+    image = cv2.imread(args.image)
+    # load the class labels from disk
+    rows = open(args.labels).read().strip().split("\n")
+    classes = [r[r.find(" ") + 1:].split(",")[0] for r in rows]
 
-# load the input image from disk
-image = cv2.imread(args["image"])
+    # our CNN requires fixed spatial dimensions for our input image(s)
+    # so we need to ensure it is resized to 224x224 pixels while
+    # performing mean subtraction (104, 117, 123) to normalize the input;
+    # after executing this command our "blob" now has the shape:
+    # (1, 3, 224, 224)
+    blob = cv2.dnn.blobFromImage(image, 1, (224, 224), (104, 117, 123))
 
-# load the class labels from disk
-rows = open(args["labels"]).read().strip().split("\n")
-classes = [r[r.find(" ") + 1:].split(",")[0] for r in rows]
+    # load our serialized model from disk
+    print("[INFO] loading model...", file=sys.stderr)
+    net = cv2.dnn.readNetFromCaffe(args.prototxt, args.model)
 
-# our CNN requires fixed spatial dimensions for our input image(s)
-# so we need to ensure it is resized to 224x224 pixels while
-# performing mean subtraction (104, 117, 123) to normalize the input;
-# after executing this command our "blob" now has the shape:
-# (1, 3, 224, 224)
-blob = cv2.dnn.blobFromImage(image, 1, (224, 224), (104, 117, 123))
+    # set the blob as input to the network and perform a forward-pass to
+    # obtain our output classification
+    net.setInput(blob)
+    start = time.time()
+    preds = net.forward()
+    end = time.time()
+    total_time = end-start
+    print("[INFO] classification took {:.5} seconds".format(total_time), file=sys.stderr)
 
-# load our serialized model from disk
-print("[INFO] loading model...")
-net = cv2.dnn.readNetFromCaffe(args["prototxt"], args["model"])
+    # sort the indexes of the probabilities in descending order (higher
+    # probabilitiy first) and grab the top-5 predictions
+    preds = preds.reshape(1, 1000)
+    idxs = np.argsort(preds[0])[::-1][:5]
 
-# set the blob as input to the network and perform a forward-pass to
-# obtain our output classification
-net.setInput(blob)
-start = time.time()
-preds = net.forward()
-end = time.time()
-print("[INFO] classification took {:.5} seconds".format(end - start))
+    result_labels = []
+    probabilities = []
+    # loop over the top-5 predictions and display them
+    for (i, idx) in enumerate(idxs):
+        # draw the top prediction on the input image
+        if i == 0:
+            text = "Label: {}, {:.2f}%".format(classes[idx], preds[0][idx] * 100)
+            cv2.putText(image, text, (5, 25), cv2.FONT_HERSHEY_SIMPLEX, 0.7, (0, 0, 255), 2)
 
-# sort the indexes of the probabilities in descending order (higher
-# probabilitiy first) and grab the top-5 predictions
-preds = preds.reshape(1, 1000)
-idxs = np.argsort(preds[0])[::-1][:5]
+        # display the predicted label + associated probability to the
+        # console
+        print("[INFO] {}. label: {}, probability: {:.5}".format(i + 1, classes[idx], preds[0][idx]), file=sys.stderr)
+        result_labels.append(classes[idx])
+        probabilities.append(preds[0][idx])
 
-# loop over the top-5 predictions and display them
-for (i, idx) in enumerate(idxs):
-    # draw the top prediction on the input image
-    if i == 0:
-        text = "Label: {}, {:.2f}%".format(classes[idx], preds[0][idx] * 100)
-        cv2.putText(image, text, (5, 25), cv2.FONT_HERSHEY_SIMPLEX, 0.7, (0, 0, 255), 2)
+    return image, total_time, labels, probabilities
 
-    # display the predicted label + associated probability to the
-    # console
-    print("[INFO] {}. label: {}, probability: {:.5}".format(i + 1, classes[idx], preds[0][idx]))
 
-# display the output image
-cv2.imshow("Image", image)
-cv2.waitKey(0)
+def parse_args():
+    # construct the argument parse and parse the arguments
+    ap = argparse.ArgumentParser()
+    ap.add_argument("-i", "--image", required=True,
+                    help="path to input image")
+    ap.add_argument("-p", "--prototxt", required=True,
+                    help="path to Caffe 'deploy' prototxt file")
+    ap.add_argument("-m", "--model", required=True,
+                    help="path to Caffe pre-trained model")
+    ap.add_argument("-l", "--labels", required=True,
+                    help="path to ImageNet labels (i.e., synsets)")
+    ap.add_argument("-v", "--verbose", action="store_true",
+                    help="print additional information to stderr")
+    return ap.parse_args()
+
+
+if __name__ == "__main__":
+    args = parse_args()
+    image, total_time, labels, probabilities = classify(args)
+
+    # display the output image
+    cv2.imshow("Image", image)
+    cv2.waitKey(0)
